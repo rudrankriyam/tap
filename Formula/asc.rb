eefc24fdc0f39326f8548f09f6f8e6b52ca79e6d
Rudrank Riyam <rudrankriyam@gmail.com>
2026-03-20 06:54:58 +0000

    Update asc to 0.44.2

--- Formula/asc.rb
+++ Formula/asc.rb
@@ -4,15 +4,15 @@
 class Asc < Formula
   desc "A fast, AI-agent friendly CLI for App Store Connect"
   homepage 'https://github.com/rudrankriyam/App-Store-Connect-CLI'
-  url 'https://github.com/rudrankriyam/App-Store-Connect-CLI/releases/download/0.44.1/asc_0.44.1_macOS_arm64'
-  version '0.44.1'
-  sha256 '27de8190c1b27dd4540bfd7d7b1420ff8f2afc4540e790e47f5486771fbc4f92'
+  url 'https://github.com/rudrankriyam/App-Store-Connect-CLI/releases/download/0.44.2/asc_0.44.2_macOS_arm64'
+  version '0.44.2'
+  sha256 '2d6fcbdb14932f20cb33906e369b0d86b899a0b8ec7f258080c0fe29c3c18e87'
   license 'MIT'
 
   depends_on :macos
 
   def install
-    bin.install 'asc_0.44.1_macOS_arm64' => 'asc'
+    bin.install 'asc_0.44.2_macOS_arm64' => 'asc'
   end
 
   test do
